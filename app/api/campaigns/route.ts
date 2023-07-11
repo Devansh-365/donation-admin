e3b081cc8c56d46ace45eb7524072858fb58a56f
Devansh Tiwari <devanshtiwari@Devanshs-MacBook-Air.local>
2023-07-11 13:36:54 +0530

    feat(api): add campaigns

---
+++ app/api/campaigns/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import * as z from "zod";
+
+const campaignCreateSchema = z.object({
+  title: z.string(),
+  imageUrl: z.string(),
+  status: z.boolean(),
+});
+
+export async function GET() {
+  try {
+    const campaigns = await prismadb.campaign.findMany({
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+      },
+    });
+
+    return new Response(JSON.stringify(campaigns));
+  } catch (error) {
+    return new Response(null, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const { user } = session;
+
+    const json = await req.json();
+    const body = campaignCreateSchema.parse(json);
+
+    const post = await prismadb.campaign.create({
+      data: {
+        title: body.title,
+        imageUrl: body.imageUrl,
+        donors: "0",
+        status: body.status,
+      },
+    });
+
+    return new Response(JSON.stringify(post));
+  } catch (error) {
+    console.log(error);
+    return new Response(null, { status: 500 });
+  }
+}
