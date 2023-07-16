b394aea130b12e619c67f962eede850309ac5485
Devansh Tiwari <devanshtiwari365@gmail.com>
2023-07-16 20:21:09 +0530

    fix: cols

--- app/[locale]/(dashboard)/[storeId]/page.tsx
+++ app/[locale]/(dashboard)/[storeId]/page.tsx
@@ -78,7 +78,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
       <div className="flex-1 space-y-4 p-8 pt-6">
         <Heading title={t("title")} description={t("subtitle")} />
         <Separator />
-        <div className="grid gap-4 grid-cols-4">
+        <div className="grid gap-4 grid-cols-3">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
