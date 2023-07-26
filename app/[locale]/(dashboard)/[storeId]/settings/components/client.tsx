cdc50fbc491c2a1a262aa8fe624b60580f094d74
Devansh-365 <devanshtiwari365@gmail.com>
2023-07-26 12:59:16 +0530

    feat: add store/id alert

--- app/[locale]/(dashboard)/[storeId]/settings/components/client.tsx
+++ app/[locale]/(dashboard)/[storeId]/settings/components/client.tsx
@@ -10,13 +10,13 @@ import { Button } from "@/components/ui/button";
 import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-import { Trash } from "lucide-react";
+import { Copy, Server, Trash } from "lucide-react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { useTranslations } from "next-intl";
-
-type Props = {};
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge, BadgeProps } from "@/components/ui/badge";
 
 const SettingClient = ({ data }: any) => {
   const origin = useOrigin();
@@ -40,6 +40,11 @@ const SettingClient = ({ data }: any) => {
       setLoading(false);
       setOpen(false);
     }
+  };
+
+  const onCopy = (description: string) => {
+    navigator.clipboard.writeText(description);
+    toast.success("Store / API Id copied to clipboard.");
   };
 
   return (
@@ -66,6 +71,24 @@ const SettingClient = ({ data }: any) => {
         <UserForm initialData={data} />
       </div>
       <Separator />
+      <Alert>
+        <Server className="h-4 w-4" />
+        <AlertTitle className="flex items-center gap-x-2">
+          Store / API Id
+        </AlertTitle>
+        <AlertDescription className="mt-4 flex items-center justify-between">
+          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+            {params?.storeId}
+          </code>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => onCopy(`${params?.storeId}`)}
+          >
+            <Copy className="h-4 w-4" />
+          </Button>
+        </AlertDescription>
+      </Alert>
       <ApiAlert
         title="NEXT_PUBLIC_API_URL"
         variant="public"
